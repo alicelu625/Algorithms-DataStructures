804be9f1d3afe121b0016823be233831f54e57b1
Alice <alicelu625@gmail.com>
2020-11-12 23:51:24 -0800

    Python solution for longest substring

---
+++ Sliding_Window_Algorithm/longest_substring_no_repeat.py
@@ -0,0 +1,23 @@
+#From LeetCode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+#Given a string, find the length of the longest substring without repeated characters
+#Solution: Use Sliding Windows technique. Runtime: O(n)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        head = 0 #start of substring
+        maxLength = 0 #max length of substring with no repeats
+        usedChars = {} #hash used chars
+        
+        for i in range(len(s)):
+            #if char already seen before and seen in current substring,
+            #then set head to after the initial repeated char
+            if s[i] in usedChars and head <= usedChars[s[i]]:
+                head = usedChars[s[i]] + 1
+            #else max length = the longer of previous max length or current substring length
+            else:
+                maxLength = max(maxLength, i-head+1)
+            #update/add used char last seen index
+            print(head)
+            print(maxLength)
+            usedChars[s[i]] = i
+
+        return maxLength
